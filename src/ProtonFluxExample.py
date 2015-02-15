d832a2897dd0d4a927c9f9dc18189c2071a24083
Dustin B <dustin.brothers@icloud.com>
2015-02-14 19:36:17 -0700

    Altered Proton Flux Example to have a gradient slew of blue hued color plots and a legend. Still need to figure out how to disable mouse scaling as it isn't needed for this application, and I also need to figure out how to change the x-axis tick labels to add the date stamps from the NOAA API.

--- src/ProtonFluxExample.py
+++ src/ProtonFluxExample.py
@@ -14,9 +14,9 @@ import NoaaApi
 
 app = QtGui.QApplication([])
 
-win = pg.GraphicsWindow(title="Basic plotting examples")
-win.resize(400,200)
-win.setWindowTitle('pyqtgraph example: Plotting')
+win = pg.GraphicsWindow()
+win.resize(800,400)
+win.setWindowTitle('Flux : Proton Range Plot')
 
 # Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
@@ -30,53 +30,72 @@ def timer_getGOESRangeProtonFlux():
     global protonFlux_curve_420_510MeV,protonFlux_curve_510_700MeV
     global protonFlux_curve_gt700MeV
     global protonFlux_data
-    protonFlux_curve_0_7_4MeV.setData(protonFlux_data["data"]["0.7-4 MeV Protons"])
-    protonFlux_curve_4_9MeV.setData(protonFlux_data["data"]["4-9 MeV Protons"])
-    protonFlux_curve_9_15MeV.setData(protonFlux_data["data"]["9-15 MeV Protons"])
-    protonFlux_curve_15_40MeV.setData(protonFlux_data["data"]["15-40 MeV Protons"])
-    protonFlux_curve_38_82MeV.setData(protonFlux_data["data"]["38-82 MeV Protons"])
-    protonFlux_curve_84_200MeV.setData(protonFlux_data["data"]["84-200 MeV Protons"])
+    protonFlux_curve_0_7_4MeV.setData(protonFlux_data  ["data"]["0.7-4 MeV Protons"  ])
+    protonFlux_curve_4_9MeV.setData(protonFlux_data    ["data"]["4-9 MeV Protons"    ])
+    protonFlux_curve_9_15MeV.setData(protonFlux_data   ["data"]["9-15 MeV Protons"   ])
+    protonFlux_curve_15_40MeV.setData(protonFlux_data  ["data"]["15-40 MeV Protons"  ])
+    protonFlux_curve_38_82MeV.setData(protonFlux_data  ["data"]["38-82 MeV Protons"  ])
+    protonFlux_curve_84_200MeV.setData(protonFlux_data ["data"]["84-200 MeV Protons" ])
     protonFlux_curve_110_900MeV.setData(protonFlux_data["data"]["110-900 MeV Protons"])
     protonFlux_curve_350_420MeV.setData(protonFlux_data["data"]["350-420 MeV Protons"])
     protonFlux_curve_420_510MeV.setData(protonFlux_data["data"]["420-510 MeV Protons"])
     protonFlux_curve_510_700MeV.setData(protonFlux_data["data"]["510-700 MeV Protons"])
-    protonFlux_curve_gt700MeV.setData(protonFlux_data["data"][">700 MeV Protons"])
+    protonFlux_curve_gt700MeV.setData(protonFlux_data  ["data"][">700 MeV Protons"   ])
     protonFlux_data = NoaaApi.getGOESRangeProtonFlux()
 
 # Create the Proton Flux Graph Instance
 protonFlux = win.addPlot(title="Proton Flux")
 protonFlux.setLogMode(y=True)
-# One "plot" for each energy of protons
-protonFlux_curve_0_7_4MeV   = protonFlux.plot(pen='r')
-protonFlux_curve_4_9MeV     = protonFlux.plot(pen='b')
-protonFlux_curve_9_15MeV    = protonFlux.plot(pen='g')
-protonFlux_curve_15_40MeV   = protonFlux.plot(pen='y')
-protonFlux_curve_38_82MeV   = protonFlux.plot(pen='r')
-protonFlux_curve_84_200MeV  = protonFlux.plot(pen='b')
-protonFlux_curve_110_900MeV = protonFlux.plot(pen='g')
-protonFlux_curve_350_420MeV = protonFlux.plot(pen='y')
-protonFlux_curve_420_510MeV = protonFlux.plot(pen='r')
-protonFlux_curve_510_700MeV = protonFlux.plot(pen='b')
-protonFlux_curve_gt700MeV   = protonFlux.plot(pen='g')
+protonFlux.showGrid(x=True,y=True,alpha=0.3)
+
 # Grab initial plot data, contains all Proton Flux Energies
 protonFlux_data = NoaaApi.getGOESRangeProtonFlux()
+
+# One "plot" for each energy of protons
+protonFlux_curve_0_7_4MeV   = protonFlux.plot(pen="4e98b0")
+protonFlux_curve_4_9MeV     = protonFlux.plot(pen="2d809b")
+protonFlux_curve_9_15MeV    = protonFlux.plot(pen="0e7495")
+protonFlux_curve_15_40MeV   = protonFlux.plot(pen="075975")
+protonFlux_curve_38_82MeV   = protonFlux.plot(pen="03455b")
+protonFlux_curve_84_200MeV  = protonFlux.plot(pen="5680b7")
+protonFlux_curve_110_900MeV = protonFlux.plot(pen="3565a3")
+protonFlux_curve_350_420MeV = protonFlux.plot(pen="16519d")
+protonFlux_curve_420_510MeV = protonFlux.plot(pen="0d3d7a")
+protonFlux_curve_510_700MeV = protonFlux.plot(pen="072e60")
+protonFlux_curve_gt700MeV   = protonFlux.plot(pen="4DB59B")
+
+# Create the legend
+protonLegend = pg.LegendItem()
+protonLegend.setParentItem(protonFlux)
+protonLegend.addItem(protonFlux_curve_0_7_4MeV,   "0.7-4 MeV Protons")
+protonLegend.addItem(protonFlux_curve_4_9MeV,     "4-9 MeV Protons")
+protonLegend.addItem(protonFlux_curve_9_15MeV,    "9-15 MeV Protons")
+protonLegend.addItem(protonFlux_curve_15_40MeV,   "15-40 MeV Protons")
+protonLegend.addItem(protonFlux_curve_38_82MeV,   "38-82 MeV Protons")
+protonLegend.addItem(protonFlux_curve_84_200MeV,  "84-200 MeV Protons")
+protonLegend.addItem(protonFlux_curve_110_900MeV, "110-900 MeV Protons")
+protonLegend.addItem(protonFlux_curve_350_420MeV, "350-420 MeV Protons")
+protonLegend.addItem(protonFlux_curve_420_510MeV, "420-510 MeV Protons")
+protonLegend.addItem(protonFlux_curve_510_700MeV, "510-700 MeV Protons")
+protonLegend.addItem(protonFlux_curve_gt700MeV,   ">700 MeV Protons")
+
 # Plot initial
-protonFlux_curve_0_7_4MeV.setData(protonFlux_data["data"]["0.7-4 MeV Protons"])
-protonFlux_curve_4_9MeV.setData(protonFlux_data["data"]["4-9 MeV Protons"])
-protonFlux_curve_9_15MeV.setData(protonFlux_data["data"]["9-15 MeV Protons"])
-protonFlux_curve_15_40MeV.setData(protonFlux_data["data"]["15-40 MeV Protons"])
-protonFlux_curve_38_82MeV.setData(protonFlux_data["data"]["38-82 MeV Protons"])
-protonFlux_curve_84_200MeV.setData(protonFlux_data["data"]["84-200 MeV Protons"])
+protonFlux_curve_0_7_4MeV.setData(protonFlux_data  ["data"]["0.7-4 MeV Protons"  ])
+protonFlux_curve_4_9MeV.setData(protonFlux_data    ["data"]["4-9 MeV Protons"    ])
+protonFlux_curve_9_15MeV.setData(protonFlux_data   ["data"]["9-15 MeV Protons"   ])
+protonFlux_curve_15_40MeV.setData(protonFlux_data  ["data"]["15-40 MeV Protons"  ])
+protonFlux_curve_38_82MeV.setData(protonFlux_data  ["data"]["38-82 MeV Protons"  ])
+protonFlux_curve_84_200MeV.setData(protonFlux_data ["data"]["84-200 MeV Protons" ])
 protonFlux_curve_110_900MeV.setData(protonFlux_data["data"]["110-900 MeV Protons"])
 protonFlux_curve_350_420MeV.setData(protonFlux_data["data"]["350-420 MeV Protons"])
 protonFlux_curve_420_510MeV.setData(protonFlux_data["data"]["420-510 MeV Protons"])
 protonFlux_curve_510_700MeV.setData(protonFlux_data["data"]["510-700 MeV Protons"])
-protonFlux_curve_gt700MeV.setData(protonFlux_data["data"][">700 MeV Protons"])
+protonFlux_curve_gt700MeV.setData(protonFlux_data  ["data"][">700 MeV Protons"   ])
 protonFlux_data = NoaaApi.getGOESRangeProtonFlux()
 
 protonFluxtimer = QtCore.QTimer()
 protonFluxtimer.timeout.connect(timer_getGOESRangeProtonFlux)
-protonFluxtimer.start(60000)
+protonFluxtimer.start(int(protonFlux_data["update"]))
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
