c0e437262ad8f741c2b80c20e1de9b96ea3a97c2
Dustin B <dustin.brothers@icloud.com>
2015-02-21 07:35:26 -0700

    Restructured the informational windows that can be accessed while the application is running. Provides more build information.

--- src/SpaceWeather.py
+++ src/SpaceWeather.py
@@ -1,14 +1,19 @@
 """
-    Full Space Weather Application. This is the top-level module that will
-    pull in all low level API Libraries, Graphing Libraries and Application
-    support Libraries.
+  Full Space Weather Application. This is the top-level module that will
+  pull in all low level API Libraries, Graphing Libraries and Application
+  support Libraries.
 
-    This application is heavily modelled after the examples provided by the
-    good people at MatPlotLib. Thank you Florent Rougon and Darren Dale for
-    your development of the embedding in Qt4 example.
+  This application is heavily modelled after the examples provided by the
+  good people at MatPlotLib. Thank you Florent Rougon and Darren Dale for
+  your development of the embedding in Qt4 example.
 
-    http://matplotlib.org/examples/index.html
+  http://matplotlib.org/examples/index.html
 """
+
+# Version of the application
+progversion = "0.1"
+progbuild = "2"
+progdate = "20150221"
 
 # Custom backend Libraries
 import NoaaApi
@@ -27,57 +32,69 @@ from matplotlib.figure import Figure
   Branch using PyQt or PySide based on MatPlotLib values.
 """
 if(qt_compat.QT_API == qt_compat.QT_API_PYSIDE):
-    from PySide import QtGui, QtCore
+  from PySide import QtGui, QtCore
 else:
-    from PyQt4 import QtGui, QtCore
+  from PyQt4 import QtGui, QtCore
 
 ###########################################################################
 # Main Application Object
 ###########################################################################
 class ApplicationWindow(QtGui.QMainWindow):
-    def __init__(self):
-        QtGui.QMainWindow.__init__(self)
-        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        self.setWindowTitle("application main window")
+  def __init__(self):
+    QtGui.QMainWindow.__init__(self)
+    self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+    self.setWindowTitle("application main window")
 
-        self.file_menu = QtGui.QMenu('&File', self)
-        self.file_menu.addAction('&Quit', self.fileQuit,
-                                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
-        self.menuBar().addMenu(self.file_menu)
+    self.file_menu = QtGui.QMenu('&File', self)
+    self.file_menu.addAction('&Quit', self.fileQuit,
+                 QtCore.Qt.CTRL + QtCore.Qt.Key_Q)
+    self.menuBar().addMenu(self.file_menu)
 
-        self.help_menu = QtGui.QMenu('&Help', self)
-        self.menuBar().addSeparator()
-        self.menuBar().addMenu(self.help_menu)
+    self.help_menu = QtGui.QMenu('&Help', self)
+    self.menuBar().addSeparator()
+    self.menuBar().addMenu(self.help_menu)
 
-        self.help_menu.addAction('&About', self.about)
+    self.help_menu.addAction('&About', self.about)
+    self.help_menu.addAction('&Version', self.version)
 
-        self.main_widget = QtGui.QWidget(self)
+    self.main_widget = QtGui.QWidget(self)
 
-        l = QtGui.QVBoxLayout(self.main_widget)
-        sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
-        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
-        l.addWidget(sc)
-        l.addWidget(dc)
+    l = QtGui.QVBoxLayout(self.main_widget)
+    # sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=300)
+    # dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=300)
+    # l.addWidget(sc)
+    # l.addWidget(dc)
 
-        self.main_widget.setFocus()
-        self.setCentralWidget(self.main_widget)
+    self.main_widget.setFocus()
+    self.setCentralWidget(self.main_widget)
 
-        self.statusBar().showMessage("All hail matplotlib!", 2000)
+    self.statusBar().showMessage("All hail matplotlib!", 2000)
 
-    def fileQuit(self):
-        self.close()
+  def fileQuit(self):
+    self.close()
 
-    def closeEvent(self, ce):
-        self.fileQuit()
+  def closeEvent(self, ce):
+    self.fileQuit()
 
-    def about(self):
-        QtGui.QMessageBox.about(self, "About",
-"""embedding_in_qt4.py example
-Copyright 2005 Florent Rougon, 2006 Darren Dale
+  def about(self):
+    QtGui.QMessageBox.about(self, "About",
+      (
+"""Space Weather Application
+ver %s
+2015 RDustinB"""%progversion)
+    )
+  def version(self):
+    QtGui.QMessageBox.about(self, "Version",
+      (
+"""Version: %s
+Build: %s
+Date: %s"""%(progversion,progbuild,progdate))
+    )
 
-This program is a simple example of a Qt4 application embedding matplotlib
-canvases.
-
-It may be used and modified with no restriction; raw copies as well as
-modified versions may be distributed without limitation."""
-)
+# Create the Application window
+qApp = QtGui.QApplication(sys.argv)
+aw = ApplicationWindow()
+aw.setWindowTitle("Space Weather Grapher")
+aw.show()
+sys.exit(qApp.exec_())
+#qApp.exec_()
