7fe526b76f9ddeb7b537e95f12e1e9afa1844484
Dustin B <dustin.brothers@icloud.com>
2015-05-23 09:42:29 -0600

    More updates working on the new Space Weather structure. Using Pandas and the code is much more Pythonic.

--- src/NewSpaceWeather.py
+++ src/NewSpaceWeather.py
@@ -2,41 +2,44 @@ import pandas as pd
 
 pd.set_option('display.mpl_style', 'default')
 
-fixed_data = pd.read_table(
-  'http://legacy-www.swpc.noaa.gov/ftpdir/lists/xray/20150310_Gp_xr_1m.txt',
-  sep=' ',
-  skipinitialspace=True,
-  parse_dates={'Time':[0,1,2,3]},
-  names=['YY','MM','DD','HHMM','Short','Long'],
-  usecols=[0,1,2,3,6,7],
-  skiprows=19,
-  na_values="-1.00e+05",
-  index_col='Time')
+# Determine Start Date
+start_date_YYYY = 2015
+start_date_MM = 3
+start_date_DD = 7
 
-fixed_data = fixed_data.append(
-  pd.read_table(
-    'http://legacy-www.swpc.noaa.gov/ftpdir/lists/xray/20150311_Gp_xr_1m.txt',
-    sep=' ',
-    skipinitialspace=True,
-    parse_dates={'Time':[0,1,2,3]},
-    names=['YY','MM','DD','HHMM','Short','Long'],
-    usecols=[0,1,2,3,6,7],
-    skiprows=19,
-    na_values="-1.00e+05",
-    index_col='Time')
-  )
+# Determine Length of Time in Days
+day_length = 10
 
-fixed_data = fixed_data.append(
-  pd.read_table(
-    'http://legacy-www.swpc.noaa.gov/ftpdir/lists/xray/20150312_Gp_xr_1m.txt',
-    sep=' ',
-    skipinitialspace=True,
-    parse_dates={'Time':[0,1,2,3]},
-    names=['YY','MM','DD','HHMM','Short','Long'],
-    usecols=[0,1,2,3,6,7],
-    skiprows=19,
-    na_values="-1.00e+05",
-    index_col='Time')
-  )
+# Loop Through Number of Days
+for dates in range(0,day_length):
+  # Calculate Date Stamp
+  date_stamp = ("%4d%02d%02d"%(start_date_YYYY,start_date_MM,(start_date_DD+dates)))
+  # Create the List
+  if(dates == 0):
+    plotData = pd.read_table(
+      ("http://legacy-www.swpc.noaa.gov/ftpdir/lists/xray/%s_Gp_xr_1m.txt"%(date_stamp)),
+      sep=' ',
+      skipinitialspace=True,
+      parse_dates={'Time':[0,1,2,3]},
+      names=['YY','MM','DD','HHMM','Short','Long'],
+      usecols=[0,1,2,3,6,7],
+      skiprows=19,
+      na_values="-1.00e+05",
+      index_col='Time'
+      )
+  else:
+    plotData = plotData.append(
+      pd.read_table(
+        ("http://legacy-www.swpc.noaa.gov/ftpdir/lists/xray/%s_Gp_xr_1m.txt"%(date_stamp)),
+        sep=' ',
+        skipinitialspace=True,
+        parse_dates={'Time':[0,1,2,3]},
+        names=['YY','MM','DD','HHMM','Short','Long'],
+        usecols=[0,1,2,3,6,7],
+        skiprows=19,
+        na_values="-1.00e+05",
+        index_col='Time'
+        )
+      )
 
-fixed_data.plot(figsize=(15, 10), legend=True, logy=True, stacked=True, title="xRay Flux", rot=45).yaxis.grid(True, which='Minor')
+plotData.plot(figsize=(15, 10), legend=True, logy=True, stacked=True, title="xRay Flux", rot=45).yaxis.grid(True, which='Minor')
