230ac375bc803c9fe4bb5066826502350956c888
Dustin Brothers <tools_2018@crypticvapor.com>
2020-05-21 15:45:23 -0600

    Added error logging and recovery for the solar regions and sunspot report data metric functions

--- src/new/fetch_summary.py
+++ src/new/fetch_summary.py
@@ -536,7 +536,10 @@ def get_measurement_xrays(period, error_log_in, data_in, date):
 ###############################################################################
 # The Sun Itself
 ###############################################################################
-def get_solar_regions():
+def get_solar_regions(error_log_in, data_in, date):
+  # This function calls' error log and data are passed in to be updated, this allows the error handling to "recover" by
+  # simply passing back the data that was in the json archive previously, keeping the stored data
+  error_log = error_log_in
   # observed_date
   # region
   # latitude
@@ -569,44 +572,82 @@ def get_solar_regions():
   if DEBUG: print("get_solar_regions()")
   url = "https://services.swpc.noaa.gov/json/solar_regions.json"
   
-  with urllib.request.urlopen(url) as thisurl:
-    data = json.loads(thisurl.read().decode())
-  if DEBUG: print("Example region                  : %s"%(data[0]["region"]))
-  if DEBUG: print("Example latitude                : %s"%(data[0]["latitude"]))
-  if DEBUG: print("Example longitude               : %s"%(data[0]["longitude"]))
-  if DEBUG: print("Example location                : %s"%(data[0]["location"]))
-  if DEBUG: print("Example carrington_longitude    : %s"%(data[0]["carrington_longitude"]))
-  if DEBUG: print("Example old_carrington_longitude: %s"%(data[0]["old_carrington_longitude"]))
-  if DEBUG: print("Example area                    : %s"%(data[0]["area"]))
-  if DEBUG: print("Example spot_class              : %s"%(data[0]["spot_class"]))
-  if DEBUG: print("Example extent                  : %s"%(data[0]["extent"]))
-  if DEBUG: print("Example number_spots            : %s"%(data[0]["number_spots"]))
-  if DEBUG: print("Example mag_class               : %s"%(data[0]["mag_class"]))
-  if DEBUG: print("Example mag_string              : %s"%(data[0]["mag_string"]))
-  if DEBUG: print("Example status                  : %s"%(data[0]["status"]))
-  if DEBUG: print("Example c_xray_events           : %s"%(data[0]["c_xray_events"]))
-  if DEBUG: print("Example m_xray_events           : %s"%(data[0]["m_xray_events"]))
-  if DEBUG: print("Example x_xray_events           : %s"%(data[0]["x_xray_events"]))
-  if DEBUG: print("Example proton_events           : %s"%(data[0]["proton_events"]))
-  if DEBUG: print("Example s_flares                : %s"%(data[0]["s_flares"]))
-  if DEBUG: print("Example impulse_flares_1        : %s"%(data[0]["impulse_flares_1"]))
-  if DEBUG: print("Example impulse_flares_2        : %s"%(data[0]["impulse_flares_2"]))
-  if DEBUG: print("Example impulse_flares_3        : %s"%(data[0]["impulse_flares_3"]))
-  if DEBUG: print("Example impulse_flares_4        : %s"%(data[0]["impulse_flares_4"]))
-  if DEBUG: print("Example protons                 : %s"%(data[0]["protons"]))
-  if DEBUG: print("Example c_flare_probability     : %s"%(data[0]["c_flare_probability"]))
-  if DEBUG: print("Example m_flare_probability     : %s"%(data[0]["m_flare_probability"]))
-  if DEBUG: print("Example x_flare_probability     : %s"%(data[0]["x_flare_probability"]))
-  if DEBUG: print("Example proton_probability      : %s"%(data[0]["proton_probability"]))
-  if DEBUG: print("Example first_date              : %s"%(data[0]["first_date"]))
-  if DEBUG: print("Example observed_date[0]        : %s"%(data[0]["observed_date"]))
-  if DEBUG: print("Example observed_date[-1]       : %s"%(data[-1]["observed_date"]))
-  if DEBUG: print("\n")
-  
-  return data
+  # Create the error dict if necessary
+  if "get_solar_regions()" not in error_log:
+    error_log["get_solar_regions()"] = dict()
+    error_log["get_solar_regions()"]["urlopen"] = list()
+    error_log["get_solar_regions()"]["corrupt"] = list()
+    error_log["get_solar_regions()"]["time_tag"] = list()
 
-def get_sunspot_report():
-  # Get the Sunspot Report
+  # Update the datestamp list
+  error_log["get_solar_regions()"]["time_tag"].append(date)
+
+  # Make sure we access the server cleanly
+  try:
+    with urllib.request.urlopen(url) as thisurl:
+      tmp_data = json.loads(thisurl.read().decode())
+    error_log["get_solar_regions()"]["urlopen"].append(False)
+  except:
+    # If Opening the URL is an issue, then append a False flag for the data corruption since it isn't being tested
+    error_log["get_solar_regions()"]["urlopen"].append(True)
+    error_log["get_solar_regions()"]["corrupt"].append(False)
+    # If we cannot even fetch the data, just return what was passed to us....
+    return (error_log, data_in)
+
+  # Test that the data looks valid before passing
+  try:
+    if DEBUG: print("Example region                  : %s"%(tmp_data[0]["region"]))
+    if DEBUG: print("Example latitude                : %s"%(tmp_data[0]["latitude"]))
+    if DEBUG: print("Example longitude               : %s"%(tmp_data[0]["longitude"]))
+    if DEBUG: print("Example location                : %s"%(tmp_data[0]["location"]))
+    if DEBUG: print("Example carrington_longitude    : %s"%(tmp_data[0]["carrington_longitude"]))
+    if DEBUG: print("Example old_carrington_longitude: %s"%(tmp_data[0]["old_carrington_longitude"]))
+    if DEBUG: print("Example area                    : %s"%(tmp_data[0]["area"]))
+    if DEBUG: print("Example spot_class              : %s"%(tmp_data[0]["spot_class"]))
+    if DEBUG: print("Example extent                  : %s"%(tmp_data[0]["extent"]))
+    if DEBUG: print("Example number_spots            : %s"%(tmp_data[0]["number_spots"]))
+    if DEBUG: print("Example mag_class               : %s"%(tmp_data[0]["mag_class"]))
+    if DEBUG: print("Example mag_string              : %s"%(tmp_data[0]["mag_string"]))
+    if DEBUG: print("Example status                  : %s"%(tmp_data[0]["status"]))
+    if DEBUG: print("Example c_xray_events           : %s"%(tmp_data[0]["c_xray_events"]))
+    if DEBUG: print("Example m_xray_events           : %s"%(tmp_data[0]["m_xray_events"]))
+    if DEBUG: print("Example x_xray_events           : %s"%(tmp_data[0]["x_xray_events"]))
+    if DEBUG: print("Example proton_events           : %s"%(tmp_data[0]["proton_events"]))
+    if DEBUG: print("Example s_flares                : %s"%(tmp_data[0]["s_flares"]))
+    if DEBUG: print("Example impulse_flares_1        : %s"%(tmp_data[0]["impulse_flares_1"]))
+    if DEBUG: print("Example impulse_flares_2        : %s"%(tmp_data[0]["impulse_flares_2"]))
+    if DEBUG: print("Example impulse_flares_3        : %s"%(tmp_data[0]["impulse_flares_3"]))
+    if DEBUG: print("Example impulse_flares_4        : %s"%(tmp_data[0]["impulse_flares_4"]))
+    if DEBUG: print("Example protons                 : %s"%(tmp_data[0]["protons"]))
+    if DEBUG: print("Example c_flare_probability     : %s"%(tmp_data[0]["c_flare_probability"]))
+    if DEBUG: print("Example m_flare_probability     : %s"%(tmp_data[0]["m_flare_probability"]))
+    if DEBUG: print("Example x_flare_probability     : %s"%(tmp_data[0]["x_flare_probability"]))
+    if DEBUG: print("Example proton_probability      : %s"%(tmp_data[0]["proton_probability"]))
+    if DEBUG: print("Example first_date              : %s"%(tmp_data[0]["first_date"]))
+    if DEBUG: print("Example observed_date[0]        : %s"%(tmp_data[0]["observed_date"]))
+    if DEBUG: print("Example observed_date[-1]       : %s"%(tmp_data[-1]["observed_date"]))
+    if DEBUG: print("Total data points               : %d"%(len(tmp_data)))
+    if DEBUG: print("\n")
+  
+    # If this doesn't throw an exception but doesn't meet these conditions, the data is corrupt
+    if (len(tmp_data[0]) is not 29) or (len(tmp_data[-1]) is not 29):
+      error_log["get_solar_regions()"]["corrupt"].append(True)
+      return (error_log, data_in)
+
+    # Format this data key
+    data["get_solar_regions()"] = listOfDicts_to_dictOfLists(tmp_data)
+
+    error_log["get_solar_regions()"]["corrupt"].append(False)
+    return (error_log, data)
+  except:
+    error_log["get_solar_regions()"]["corrupt"].append(True)
+    # If the data appears corrupt from some basic tests, return the original data
+    return (error_log, data_in)
+
+def get_sunspot_report(error_log_in, data_in, date):
+  # This function calls' error log and data are passed in to be updated, this allows the error handling to "recover" by
+  # simply passing back the data that was in the json archive previously, keeping the stored data
+  error_log = error_log_in
   # "time_tag":"2020-05-01T05:50:00"
   # "Obsdate":"2020-05-01T00:00:00"
   # "Obstime":"0550"
@@ -636,40 +677,74 @@ def get_sunspot_report():
   if DEBUG: print("get_sunspot_report()")
   url = "https://services.swpc.noaa.gov/json/sunspot_report.json"
   
-  with urllib.request.urlopen(url) as thisurl:
-    data = json.loads(thisurl.read().decode())
+  # Create the error dict if necessary
+  if "get_sunspot_report()" not in error_log:
+    error_log["get_sunspot_report()"] = dict()
+    error_log["get_sunspot_report()"]["urlopen"] = list()
+    error_log["get_sunspot_report()"]["corrupt"] = list()
+    error_log["get_sunspot_report()"]["time_tag"] = list()
+
+  # Update the datestamp list
+  error_log["get_sunspot_report()"]["time_tag"].append(date)
+
+  # Make sure we access the server cleanly
+  try:
+    with urllib.request.urlopen(url) as thisurl:
+      tmp_data = json.loads(thisurl.read().decode())
+    error_log["get_sunspot_report()"]["urlopen"].append(False)
+  except:
+    # If Opening the URL is an issue, then append a False flag for the data corruption since it isn't being tested
+    error_log["get_sunspot_report()"]["urlopen"].append(True)
+    error_log["get_sunspot_report()"]["corrupt"].append(False)
+    # If we cannot even fetch the data, just return what was passed to us....
+    return (error_log, data_in)
+
+  # Test that the data looks valid before passing
+  try:
+    if DEBUG: print("Example Obsdate          : %s"%(tmp_data[0]["Obsdate"]))
+    if DEBUG: print("Example Obstime          : %s"%(tmp_data[0]["Obstime"]))
+    if DEBUG: print("Example Station          : %s"%(tmp_data[0]["Station"]))
+    if DEBUG: print("Example Observatory      : %s"%(tmp_data[0]["Observatory"]))
+    if DEBUG: print("Example Type             : %s"%(tmp_data[0]["Type"]))
+    if DEBUG: print("Example Quality          : %s"%(tmp_data[0]["Quality"]))
+    if DEBUG: print("Example Region           : %s"%(tmp_data[0]["Region"]))
+    if DEBUG: print("Example Latitude         : %s"%(tmp_data[0]["Latitude"]))
+    if DEBUG: print("Example Report_Longitude : %s"%(tmp_data[0]["Report_Longitude"]))
+    if DEBUG: print("Example Longitude        : %s"%(tmp_data[0]["Longitude"]))
+    if DEBUG: print("Example Report_Location  : %s"%(tmp_data[0]["Report_Location"]))
+    if DEBUG: print("Example Location         : %s"%(tmp_data[0]["Location"]))
+    if DEBUG: print("Example Carlon           : %s"%(tmp_data[0]["Carlon"]))
+    if DEBUG: print("Example Extent           : %s"%(tmp_data[0]["Extent"]))
+    if DEBUG: print("Example Area             : %s"%(tmp_data[0]["Area"]))
+    if DEBUG: print("Example Numspot          : %s"%(tmp_data[0]["Numspot"]))
+    if DEBUG: print("Example Zurich           : %s"%(tmp_data[0]["Zurich"]))
+    if DEBUG: print("Example Penumbra         : %s"%(tmp_data[0]["Penumbra"]))
+    if DEBUG: print("Example Compact          : %s"%(tmp_data[0]["Compact"]))
+    if DEBUG: print("Example Spotclass        : %s"%(tmp_data[0]["Spotclass"]))
+    if DEBUG: print("Example Magcode          : %s"%(tmp_data[0]["Magcode"]))
+    if DEBUG: print("Example Magclass         : %s"%(tmp_data[0]["Magclass"]))
+    if DEBUG: print("Example Obsid            : %s"%(tmp_data[0]["Obsid"]))
+    if DEBUG: print("Example Report_Status    : %s"%(tmp_data[0]["Report_Status"]))
+    if DEBUG: print("Example ValidSpotClass   : %s"%(tmp_data[0]["ValidSpotClass"]))
+    if DEBUG: print("Example time_tag[0]      : %s"%(tmp_data[0]["time_tag"]))
+    if DEBUG: print("Example time_tag[-1]     : %s"%(tmp_data[-1]["time_tag"]))
+    if DEBUG: print("Total data points        : %d"%(len(tmp_data)))
+    if DEBUG: print("\n")
   
-  if DEBUG: print("Example Obsdate:          %s"%(data[0]["Obsdate"]))
-  if DEBUG: print("Example Obstime:          %s"%(data[0]["Obstime"]))
-  if DEBUG: print("Example Station:          %s"%(data[0]["Station"]))
-  if DEBUG: print("Example Observatory:      %s"%(data[0]["Observatory"]))
-  if DEBUG: print("Example Type:             %s"%(data[0]["Type"]))
-  if DEBUG: print("Example Quality:          %s"%(data[0]["Quality"]))
-  if DEBUG: print("Example Region:           %s"%(data[0]["Region"]))
-  if DEBUG: print("Example Latitude:         %s"%(data[0]["Latitude"]))
-  if DEBUG: print("Example Report_Longitude: %s"%(data[0]["Report_Longitude"]))
-  if DEBUG: print("Example Longitude:        %s"%(data[0]["Longitude"]))
-  if DEBUG: print("Example Report_Location:  %s"%(data[0]["Report_Location"]))
-  if DEBUG: print("Example Location:         %s"%(data[0]["Location"]))
-  if DEBUG: print("Example Carlon:           %s"%(data[0]["Carlon"]))
-  if DEBUG: print("Example Extent:           %s"%(data[0]["Extent"]))
-  if DEBUG: print("Example Area:             %s"%(data[0]["Area"]))
-  if DEBUG: print("Example Numspot:          %s"%(data[0]["Numspot"]))
-  if DEBUG: print("Example Zurich:           %s"%(data[0]["Zurich"]))
-  if DEBUG: print("Example Penumbra:         %s"%(data[0]["Penumbra"]))
-  if DEBUG: print("Example Compact:          %s"%(data[0]["Compact"]))
-  if DEBUG: print("Example Spotclass:        %s"%(data[0]["Spotclass"]))
-  if DEBUG: print("Example Magcode:          %s"%(data[0]["Magcode"]))
-  if DEBUG: print("Example Magclass:         %s"%(data[0]["Magclass"]))
-  if DEBUG: print("Example Obsid:            %s"%(data[0]["Obsid"]))
-  if DEBUG: print("Example Report_Status:    %s"%(data[0]["Report_Status"]))
-  if DEBUG: print("Example ValidSpotClass:   %s"%(data[0]["ValidSpotClass"]))
-  if DEBUG: print("Example time_tag[0]:      %s"%(data[0]["time_tag"]))
-  if DEBUG: print("Example time_tag[-1]:     %s"%(data[-1]["time_tag"]))
-  if DEBUG: print("Total data points: %d"%(len(data)))
-  if DEBUG: print("\n")
-  
-  return data
+    # If this doesn't throw an exception but doesn't meet these conditions, the data is corrupt
+    if (len(tmp_data[0]) is not 26) or (len(tmp_data[-1]) is not 26):
+      error_log["get_sunspot_report()"]["corrupt"].append(True)
+      return (error_log, data_in)
+
+    # Format this data key
+    data["get_sunspot_report()"] = listOfDicts_to_dictOfLists(tmp_data)
+
+    error_log["get_sunspot_report()"]["corrupt"].append(False)
+    return (error_log, data)
+  except:
+    error_log["get_sunspot_report()"]["corrupt"].append(True)
+    # If the data appears corrupt from some basic tests, return the original data
+    return (error_log, data_in)
 
 ###############################################################################
 # Support Functions
@@ -750,8 +825,8 @@ if __name__ == "__main__":
   ENWEATHERMEASURES6x  = False
   ENWEATHERMEASURES1x  = False
   ENWEATHERMEASURES3x  = False
-  ENWEATHERMEASURES7x  = True
-  ENSUNMEASURES        = False
+  ENWEATHERMEASURES7x  = False
+  ENSUNMEASURES        = True
 
   # Fetch the current data and error log
   if ENFILEWRITES:         data = fetch_archive('data.json')
@@ -896,9 +971,21 @@ if __name__ == "__main__":
                            data.pop("get_measurement_xrays()", None)
                            error_log.pop("get_measurement_xrays()", None)
 
-  # Old Data and Error Collection Framework
-  if ENSUNMEASURES:        data["get_solar_regions()"]                        = listOfDicts_to_dictOfLists(get_solar_regions())
-  if ENSUNMEASURES:        data["get_sunspot_report()"]                       = listOfDicts_to_dictOfLists(get_sunspot_report())
+  if ENSUNMEASURES: # Catch the data and the error log
+                           all_info                                           = get_solar_regions(error_log, data, now)
+                           error_log                                          = all_info[0]
+                           data                                               = all_info[1]
+  elif RECORDERRORS:       # Strip the data and error log if this data type is no longer being sampled
+                           data.pop("get_solar_regions()", None)
+                           error_log.pop("get_solar_regions()", None)
+
+  if ENSUNMEASURES: # Catch the data and the error log
+                           all_info                                           = get_sunspot_report(error_log, data, now)
+                           error_log                                          = all_info[0]
+                           data                                               = all_info[1]
+  elif RECORDERRORS:       # Strip the data and error log if this data type is no longer being sampled
+                           data.pop("get_sunspot_report()", None)
+                           error_log.pop("get_sunspot_report()", None)
 
   # Store the updated data and error_log
   if ENFILEWRITES:         store_archive('data.json', data)
