56a9becd0befba1ee2a7a18bead54f23b420b2db
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-10-01 15:41:28 -0600

    Updates to data agent to allow for storing trimmed data and also reducing the number of decimal places of the floats stored.

--- src/data_agent_master.py
+++ src/data_agent_master.py
@@ -14,7 +14,11 @@ config.read('config.ini')
 pullAndUseLocalData         = config.getboolean('general', 'use_local')
 localRawDataFolder          = config.get('general', 'local_raw')
 localFormattedDataFolder    = config.get('general', 'local_formatted')
+trimData                    = config.getboolean('data', 'trim_data')
+dataPrecision               = config.get('data', 'precision')
 allDataSourceURLs           = config.get('sources', 'urls').split()
+
+dataPrecisionFormatter = "{:.%sf}"%(dataPrecision)
 
 ################################
 # Fetch the data
@@ -31,16 +35,26 @@ for thisDataSourceURL in allDataSourceURLs:
     ################
     # Create an array of the energies...
     print("Formatting data from %s"%(thisDataSourceURL))
+    thisFirstEntryIndicator = ""
     for thisElement in thisJsonData:
-        # Create the dictionary element of the date as a list
+        # If the time_tag hasn't been created in the dictionary yet, create it
         if "time_tag" not in dataDict:
             dataDict["time_tag"] = list()
-        dataDict["time_tag"].append(thisElement["time_tag"])
+            thisFirstEntryIndicator = thisElement["energy"]
+        # Only append a new time_tag when the list element is the same type as the first one, this prevents duplicate
+        # time_tag entries due to multiple data sets in the same file.
+        elif thisFirstEntryIndicator == thisElement["energy"]:
+            dataDict["time_tag"].append(thisElement["time_tag"])
         # Create the dictionary element of this energy level as a list
         if thisElement["energy"] not in dataDict:
             dataDict[thisElement["energy"]] = list()
         # Append to the list
-        dataDict[thisElement["energy"]].append(thisElement["flux"])
+        dataDict[thisElement["energy"]].append(float(dataPrecisionFormatter.format(thisElement["flux"])))
+    ################
+    # Trim data to a small set
+    if trimData:
+        for thisKey in dataDict:
+            dataDict[thisKey] = dataDict[thisKey][:5]
     ################
     # Store the Formatted Data
     filehandling.setLocalData(localDataFolder=localFormattedDataFolder, localDataFilename=thisDataFilename, jsonData=dataDict)
