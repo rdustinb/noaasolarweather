31cc3172f2c75ac42cd279102fb9e35993f864db
Dustin Brothers <gentian.limited-0t@icloud.com>
2024-10-24 17:06:18 -0600

    Updates to view agent to allow for conversion to the local timezone based on a config.ini setting.

--- src/support/timestamp.py
+++ src/support/timestamp.py
@@ -1,7 +1,14 @@
 from datetime import datetime
+from dateutil import tz
 from tzlocal import get_localzone
 
 def getTimestamp():
     myLocalTimezone = get_localzone()
     myTimestamp = datetime.now(myLocalTimezone).strftime("%H:%M:%S %Z %Y-%m-%d")
     return myTimestamp
+
+def convertTimestamps(theseTimestamps):
+    newTimestamps = list()
+    for thisTimestamp in theseTimestamps:
+        newTimestamps.append(str(datetime.fromisoformat(thisTimestamp).astimezone(get_localzone())))
+    return newTimestamps
