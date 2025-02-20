3d269f9e5a877923a1e0aaf975672dcc171a8c5b
Dustin Brothers <gentian.limited-0t@icloud.com>
2025-02-19 18:54:17 -0700

    This seems to work, the docker container needs to be run with the environment vairable TZ set to the specific user's timezone, otherwise the view agent will show UTC time no matter what: docker run -e TZ='America/Denver' -p 8000:8000 --name test noaa-view-agent. For now the data needs to be updated through the docker shell, but that is just a proof of concept step. Ideally the data_agent would run as a cronjob inside the docker container.

--- src/view_agent.py
+++ src/view_agent.py
@@ -130,4 +130,4 @@ def update_graph_settings(n):
     return dbc_Row_array
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', port='8050', debug=True)
+    app.run_server(host='0.0.0.0', port='8080', debug=True)
